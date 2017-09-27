32fbffe898542943977a910f07fe9e8d3a537b79
tim <huangshanbo@wanda.cn>
2017-09-27 23:39:11 +0800

    resolve in ie10/11 input/others trigger onChangeValidate after set initialValue

--- src/createBaseForm.js
+++ src/createBaseForm.js
@@ -99,14 +99,17 @@ function createBaseForm(option = {}, mixins = []) {
           name = nameKeyObj.name;
         }
         const field = this.getField(name);
-        field.value = value;
-        field.dirty = true;
-        this.validateFieldsInternal([field], {
-          action,
-          options: {
-            firstFields: !!fieldMeta.validateFirst,
-          },
-        });
+        if (value || field.value) {
+          field.value = value;
+          field.dirty = true;
+          this.validateFieldsInternal([field], {
+            action,
+            options: {
+              firstFields: !!fieldMeta.validateFirst,
+            },
+          });
+        }
+
       },
 
       getCacheBind(name, action, fn) {
